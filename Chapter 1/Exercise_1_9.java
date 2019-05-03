fe0b39aed0ad37be0e61b60e2ddc03f96ae0c44c
MatthewShelley240 <49542081+MatthewShelley240@users.noreply.github.com>
2019-05-03 15:25:57 -0600

    Uploaded Exercise 1-9

---
+++ Chapter 1/Exercise_1_9.java
@@ -0,0 +1,23 @@
+/*
+ * Matthew Shelley
+ * 05/03/2019
+ * Displays the area and perimeter of a Rectangle.
+ */
+public class Exercise_1_5 {
+
+	public static void main(String[] args) {
+		//Needed Variables
+		double X = (4.5 * 7.9);
+		double Y = ((4.5 * 2) + (7.9 * 2));
+		String Z = "The area is ";
+		String ZA = "The perimeter is ";
+		String ZB = "A given rectangle has a width of 4.5 and a length of 7.9."
+				+ " What is the area and perimeter of this rectangle?";
+		//Assignment
+		System.out.println(ZB);
+		System.out.println(Z + X);
+		System.out.println(ZA + Y);
+
+	}
+
+}
