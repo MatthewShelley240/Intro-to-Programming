5a3b45427d99684630fb2e0116cd241cb2b4e3dd
MatthewShelley240 <49542081+MatthewShelley240@users.noreply.github.com>
2019-05-02 18:00:21 -0600

    Added assignment 1-5

---
+++ Exercise_1_5.java
@@ -0,0 +1,15 @@
+/*
+ * Matthew Shelley
+ * 05/02/2019
+ * Computes the math problem for Exercise 1-5
+ */
+public class Exercise_1_5 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("((9.5 * 4) - (2.5 * 3)) / (45.5 - 3.5) = ");
+		System.out.println(((9.5 * 4) - (2.5 * 3)) / (45.5 - 3.5));
+
+	}
+
+}
