b88e6306e8a260db4e16ff00149081a9a4139c5d
MatthewShelley240 <49542081+MatthewShelley240@users.noreply.github.com>
2019-05-14 17:53:44 -0600

    Added exercise 3-23

---
+++ Chapter 3/Exercise_3_23.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+/*
+ * Matthew Shelley
+ * 05/14/2019
+ * Exercise 3-23, determines whether a point is located inside a rectangle.
+ */
+public class Exercise_3_23 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//rectangle size
+		final double widthStartingPoint = 0;
+		final double widthEndingPoint = 10;
+		final double lengthStartingPoint = 0;
+		final double lengthEndingPoint = 5;
+
+		//Inquires the point to be located
+		Boolean start = true;
+			do
+			{
+				System.out.println("What is the value of X and Y in your coordinate?");
+				double pointX = input.nextDouble();
+				double pointY = input.nextDouble();
+
+				//Determines if point (X,Y) is in the rectangle.
+
+				Boolean running = true;
+				do
+				{
+					final Boolean invalid1 = pointX > widthEndingPoint;
+					final Boolean invalid2 = pointX < widthStartingPoint;
+					final Boolean invalid3 = pointY > widthEndingPoint;
+					final Boolean invalid4 = pointY < widthStartingPoint;
+					
+					
+					if(invalid1 || invalid2 || invalid3 || invalid4) {
+						System.out.println("Point (" + pointX + ", " + pointY +") is not part of the rectangle.");
+						System.out.println("Would you like to do another?");
+						//Asks to restart program
+						Boolean restart = true;
+						do
+						{
+							String repeat = input.next();
+							if(repeat.compareToIgnoreCase("Yes") == 0) {
+								restart = false;
+								running = false;
+								start = true;
+							}
+							else if(repeat.compareToIgnoreCase("No") == 0) {
+								running = false;
+								restart = false;
+								System.out.println("Farewell.");
+								start = false;
+							}
+							else {
+								System.out.println("Not understood, please try again.");
+							}
+						}while(restart);
+					}
+					else {
+						System.out.println("Point (" + pointX + ", " + pointY +") is part of the rectangle.");
+						System.out.println("Would you like to do another?");
+						Boolean restart = true;
+						do
+						{
+							String repeat = input.next();
+							if(repeat.compareToIgnoreCase("Yes") == 0) {
+								restart = false;
+								running = false;
+								start = true;
+							}
+							else if(repeat.compareToIgnoreCase("No")  == 0) {
+								running = false;
+								restart = false;
+								System.out.println("Farewell.");
+								start = false;
+							}
+							else {
+								System.out.println("Not understood, please try again.");
+							}
+						}while(restart);
+						
+					}
+
+				}while(running);
+
+			}while(start);
+
+	}
+}
