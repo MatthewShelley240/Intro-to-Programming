80abdd291ba45d7157f39feabfacf5f3121e726d
MatthewShelley240 <49542081+MatthewShelley240@users.noreply.github.com>
2019-06-07 15:12:08 -0600

    Uploaded completed program for exercise 5-17

--- Chapter 5/Exercise_5_17.java
+++ Chapter 5/Exercise_5_17.java
@@ -1,7 +1,7 @@
 /*
  * Matthew Shelley
- * 6/03/2019
- * Exercise 5-17, froms a pyramind of numbers.
+ * 6/07/2019
+ * Exercise 5-17, forms a pyramid of numbers.
  */
 
 import java.util.Scanner;
@@ -11,65 +11,56 @@ public class Exercise_5_17 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-
 		//Prompts the user to input a integer between 1 and 15.
 		System.out.println("Enter an integer between 1 and 15.");
 		int userInput = input.nextInt();
-		int lineNumber = 1;
-		
-		//Outer loop
-		boolean outerLoop = true;
-		do {
-		System.out.println(lineNumber);
-		
-		
-		//This is the 3rd loop done. Counts the right half of the pyramid.
-				boolean running = true;
+
+		//Determines if integer is valid
+		if (userInput <= 1 || userInput >= 15) {
+			System.out.println("Not understood, please try again.");
+		}
+
+		//If integer is valid prints the pyramid.
+		else {
+			int lineNumber = 1;
+
+			//Outer loop
+			boolean outerLoop = true;
+			do {
+				int counter = 0;
+
+				//This part of the code controls how many spaces happen at the beginning of each line.
+				for(int spacesCounter = lineNumber; spacesCounter < 15; spacesCounter++) 
+					System.out.print("   ");
+
+				//This part of the code controls the left side of the counting on the pyramid.
+				//For example, 15-1, or 5-1.
 				do {
-					int count = userInput;
-					
-					 int loop3 = userInput - (userInput - 1) + 1;
-					boolean running2 = true;
+					System.out.printf("%3d", lineNumber - counter);
+					counter++;
+
+				}while(counter < lineNumber);
+
+				//This part of the code controls the counting of the right side of the pyramid.
+				//For example, 2-15, or 2-5.
+				if (lineNumber > 1) {
+					int counter2 = 2;
 					do {
-						System.out.print(loop3 + " ");
-						loop3++;
-						if(loop3 > userInput)
-							running2 = false;
-					}while(running2);
-					System.out.println();
-		
-					
-		//continuation of outer loop 
-		System.out.println();
-		lineNumber++;
-		if(lineNumber > 15)
-			outerLoop = false;
-		}while(outerLoop);
-		
-		
+						System.out.printf("%3d", counter2);
+						counter2++;
+					}while (counter2 <= lineNumber);
+				}
 
 
-
-	//	}while(running);
+				//continuation of outer loop 
+				System.out.println();
+				lineNumber++;
+				if(lineNumber > userInput)
+					outerLoop = false;
+			}while(outerLoop);
+		}
 
 		System.out.print("End of Program");
 
-
-		
-		//while (lineNumber >= 1 && lineNumber <= 15) {
-		// 1st Inner loop
-
-		//System.out.printf("%3d", userInput);
-		
 	}
 }
-
-
-/* Code for loop 3
- * int start = 2
- * while (start == 2 && start <= 15) {
- * 		System.out.print(start + " ");
- * 		start++;
- * }
- * System.out.println();
- */
