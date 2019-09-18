cbb7846b29f15ba4d7c443063503b966dd09b242
MatthewShelley240 <49542081+MatthewShelley240@users.noreply.github.com>
2019-09-17 21:14:43 -0600

    Corrected and reuploaded Exercise 8-37

--- Chapter 8/Exercise_8_37.java
+++ Chapter 8/Exercise_8_37.java
@@ -1,3 +1,9 @@
+/*
+* Matthew Shelley
+* 09/17/2019
+* Exercise 8-37, tests the user's knowledge of states and capitals.
+*/
+
 import java.util.Scanner;
 
 public class Exercise_8_37 {
@@ -19,9 +25,11 @@ public class Exercise_8_37 {
 				{"Georgia", "Atlanta"},
 		};
 
-		int state = (int)Math.round(Math.random() * 10);
-		System.out.println(state);
+		//int state = (int)Math.round(Math.random() * 10);
+		//System.out.println(state);
 
+		int state = 0;
+		int totalCorrect = 0;
 
 		boolean running = true;
 		do {
@@ -30,15 +38,22 @@ public class Exercise_8_37 {
 			String capital = input.nextLine();
 
 			// Tests the user's input
-			if(statesAndCapitals[state][1].compareToIgnoreCase(capital) == 0) {
+			if(statesAndCapitals[state][1].compareToIgnoreCase(capital) == 0 && state <= 9) {
 				System.out.println("That is correct");
-				running = false;
+				state++;
+				totalCorrect++;
 			}
 			else {
 				System.out.println("That is incorrect");
+				state++;
 			}
 
-
+			if (state > 9)
+				running = false;
+			
 		}while(running);
+		
+		System.out.println("The number you got correct is: " + totalCorrect);
+		
 	}
 }
